18c34a1c0e8afa99e64f70ff19ebf431f916b220
Socolov <145919428+falcon-ov@users.noreply.github.com>
2025-06-15 15:55:26 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ info }}</h1>
+  <p>{{ someInfo }}</p>
+
+  <button type="button" @click="userData('Some new')">Отправить</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      info: "title!",
+      someInfo: "Description"
+    }
+  },
+  methods: {
+    userData(word = "new text") {
+      this.someInfo = word;
+    }
+  }
+}
+</script>
+
+<style scoped></style>
